56584634c295522ac48270f41c5ef1ba48d6e381
eiji <eijimine@gmail.com>
2017-09-25 12:51:50 -0400

    Fixed views

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jquery-rails'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'devise', '~> 4.3'
+gem 'paperclip', '~> 5.1'
 gem 'awesome_print'
 
 group :development, :test do
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,12 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all.order("created_at DESC")
+      if params[:category].blank?
+      @products = Product.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @products = Product.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -44,7 +49,7 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price_in_cents, :category_id)
+      params.require(:product).permit(:name, :description, :price_in_cents, :category_id, :product_img)
     end
 
     def find_product
